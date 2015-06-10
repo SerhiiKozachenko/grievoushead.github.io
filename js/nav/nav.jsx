c3bfa40d23dd2f40b22cee8c9d18115cf5e061f8
Sergey Kozachenko <sergiy.kozachenko@gmail.com>
2015-06-11 00:00:50 +0300

    refactor. add link to video calls

---
+++ js/nav/nav.jsx
@@ -0,0 +1,64 @@
+var Nav = React.createClass({
+
+    goHome: function(){
+      React.render(
+        <Home />,
+        document.getElementById("main_content")
+      );
+    },
+    goAbout: function(){
+      React.render(
+        <About />,
+        document.getElementById("main_content")
+      );
+    },
+    goProjects: function(){
+
+    },
+    goContact: function(){
+
+    },
+    render: function() {
+      return (
+
+        <nav className="navbar navbar-default navbar-custom navbar-fixed-top">
+          <div className="container-fluid">
+              <div className="navbar-header page-scroll">
+                  <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                      <span className="sr-only">Toggle navigation</span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                  </button>
+                  <a className="navbar-brand" href="/">Sergey Kozachenko aka Grievoushead</a>
+              </div>
+              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul className="nav navbar-nav navbar-right">
+                      <li>
+                          <a href="#/home" onClick={this.goHome}>Home</a>
+                      </li>
+                      <li>
+                          <a href="#/about" onClick={this.goAbout}>About</a>
+                      </li>
+                      <li>
+                          <a href="#/projects" onClick={this.goProjects}>Projects</a>
+                      </li>
+                      <li>
+                          <a href="#/contact" onClick={this.goContact}>Contact</a>
+                      </li>
+                      <li>
+                          <a href="//grievoushead.github.io/video-calls/">Video Call</a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+        </nav>
+
+       );
+    }
+});
+
+React.render(
+  <Nav />,
+  document.getElementById("main_nav")
+);
