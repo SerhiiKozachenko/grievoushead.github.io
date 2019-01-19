d8f9a2272e3bb608a1ae78c830ad6dbff51e6272
Sergey Kozachenko <sergiy.kozachenko@gmail.com>
2019-01-19 23:22:53 +0200

    Delete all

--- src/utils/array.js
+++
@@ -1,7 +0,0 @@
-export const groupBy = (xs, key) =>
-  xs.reduce((rv, x) => {
-    (rv[x[key]] = rv[x[key]] || []).push(x)
-    return rv;
-  }, {})
-// console.log(groupBy(['one', 'two', 'three'], 'length'));
-// => {3: ["one", "two"], 5: ["three"]}
